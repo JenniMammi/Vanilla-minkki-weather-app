a41c9a467fe38902a5dde0ec44a92e5dc4433f67
Jenni Mämmi <30490109+JenniMammi@users.noreply.github.com>
2021-08-20 12:09:10 +0300

    Added days, time in JS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayCurrentTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature-now");
@@ -5,11 +22,13 @@ function displayCurrentTemperature(response) {
   let descElement = document.querySelector("#weather-description");
   let humidElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dayElement = document.querySelector("#day");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descElement.innerHTML = response.data.weather[0].description;
   humidElement.innerHTML = Math.round(response.data.main.humidity);
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dayElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "02e63bbc86dac944d774fba2018e7b56";
